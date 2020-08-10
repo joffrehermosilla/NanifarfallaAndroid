3c5cd2d78f5f0ac294a13c1c8dc6e952cd4acecd
joffre <joffre.hermosilla@gmail.com>
2020-08-10 00:45:50 -0500

    Octavo commit Nanifarfalla Android

---
+++ app/src/main/java/nanifarfalla/app/rxjava/RX06BusActivity.java
@@ -0,0 +1,62 @@
+package nanifarfalla.app.rxjava;
+
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+
+
+public class RX06BusActivity extends AppCompatActivity {
+
+    private RX06BusFragment fragment;
+    private FragmentTransaction transaction;
+    private Button btRxBus;
+    private CompositeDisposable compositeDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx06_bus);
+
+        fragment = new RX06BusFragment();
+        transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.add( R.id.frame2,fragment );
+
+        transaction.commit();
+
+        compositeDisposable = new CompositeDisposable();
+
+        btRxBus = findViewById( R.id.btRxBus);
+        btRxBus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RX06Bus.getInstance().setEvents("Hola soy el Bus");
+            }
+        });
+
+        Observable observable = RX06Bus.getInstance().getEvents();
+        compositeDisposable.add(
+                observable.subscribe(
+                        e->{
+                            Log.d("TAG1", "RX06Activity: " + e);
+                        }
+                )
+        );
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+}
---
+++ app/src/main/java/nanifarfalla/app/rxjava/Sumar.java
@@ -0,0 +1,5 @@
+package nanifarfalla.app.rxjava;
+
+public interface Sumar {
+    int apply(int a, int b);
+}
